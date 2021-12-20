09cd6c985d6d79fbfdbb4ebbfdbd26351a3ae99c
spencerwooo <spencer.woo@outlook.com>
2021-12-21 01:22:32 +0800

    add theme toggle

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { Menu, Transition } from '@headlessui/react'
 import { Menu as MenuIcon } from 'react-feather'
 import { Fragment } from 'react'
+import Toggle from './DarkToggle'
 
 const navigations = [
   {
@@ -11,10 +12,6 @@ const navigations = [
   {
     name: 'Publication',
     link: '/publication',
-  },
-  {
-    name: 'Projects',
-    link: '/projects',
   },
   {
     name: 'Links',
@@ -33,41 +30,45 @@ const MenuItemLink = (props: { [x: string]: any; href: any; children: any }) => 
 
 const Navbar = () => {
   return (
-    <header className="flex p-6 text-gray-500 items-center justify-between dark:text-gray-400 z-50">
+    <header className="flex p-6 text-gray-500 z-50 items-center justify-between dark:text-gray-400">
       <Link href="/">Spencer</Link>
-      <nav className="flex space-x-4 hidden sm:block">
-        {navigations.map((n, i) => (
-          <Link href={n.link} key={i}>
-            {n.name}
-          </Link>
-        ))}
-      </nav>
+      <div className="flex space-x-4 items-center">
+        <nav className="flex space-x-4 items-center hidden sm:block">
+          {navigations.map((n, i) => (
+            <Link href={n.link} key={i}>
+              {n.name}
+            </Link>
+          ))}
+        </nav>
 
-      <div className="block sm:hidden">
-        <Menu as="div" className="text-left relative">
-          <Menu.Button className="text-current">
-            <MenuIcon size={20} />
-          </Menu.Button>
-          <Transition
-            as={Fragment}
-            enter="transition duration-100 ease-out"
-            enterFrom="transform scale-95 opacity-0"
-            enterTo="transform scale-100 opacity-100"
-            leave="transition duration-75 ease-out"
-            leaveFrom="transform scale-100 opacity-100"
-            leaveTo="transform scale-95 opacity-0"
-          >
-            <Menu.Items className="bg-white rounded shadow-lg mt-2 origin-top-right right-0 shadow-gray-600 w-32 absolute mobile-menu dark:(bg-dark-700)">
-              {navigations.map((n, i) => (
-                <div className="p-2" key={i}>
-                  <Menu.Item>
-                    <MenuItemLink href={n.link}>{n.name}</MenuItemLink>
-                  </Menu.Item>
-                </div>
-              ))}
-            </Menu.Items>
-          </Transition>
-        </Menu>
+        <div className="block sm:hidden">
+          <Menu as="div" className="text-left relative">
+            <Menu.Button className="flex text-current items-center">
+              <MenuIcon size={20} />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              <Menu.Items className="bg-white rounded shadow-lg mt-2 origin-top-right right-0 shadow-gray-600 w-32 absolute mobile-menu dark:(bg-dark-700)">
+                {navigations.map((n, i) => (
+                  <div className="p-2" key={i}>
+                    <Menu.Item>
+                      <MenuItemLink href={n.link}>{n.name}</MenuItemLink>
+                    </Menu.Item>
+                  </div>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+
+        <Toggle />
       </div>
     </header>
   )
