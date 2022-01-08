03156072cb2927067ed66363cdafa529dd9ec749
spencerwooo <spencer.woo@outlook.com>
2022-01-09 01:09:35 +0800

    remove referencing site manifest

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -32,7 +32,6 @@ const Post: NextPage<{ page: any; blocks: any[] }> = ({ page, blocks }) => {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <div className="flex flex-col min-h-screen dark:bg-dark-900">
