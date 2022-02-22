ad23e08c435b040d22d83189fe851cffff76b1a3
FDKevin <fdkevinzero@gmail.com>
2022-02-22 15:11:54 +0800

    update giscus repo

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,10 +4,10 @@ const Comments = () => {
   return (
     <div id="comments-section" className="border rounded border-gray-400/30 mt-4 p-4 md:-mx-4">
       <Giscus
-        repo="hduhelp/giscus-discussions"
-        repoId="R_kgDOG4vWdA"
+        repo="hduhelp/blog.hduhelp.com"
+        repoId="R_kgDOG4tEHw"
         category="Announcements"
-        categoryId="DIC_kwDOG4vWdM4CBRw2"
+        categoryId="DIC_kwDOG4tEH84CBR0z"
         mapping="pathname"
         reactionsEnabled="1"
         theme="preferred_color_scheme"
