6482229627564c14ec42f06720e127f8e7cef919
FDKevin <fdkevinzero@gmail.com>
2022-10-27 03:13:51 +0800

    update

--- functions/imaging.ts
+++
@@ -1,8 +0,0 @@
-import { getInfoFromUrl } from 'image-size-cf'
-
-const probeImageSize = async (url: string) => {
-  const dim = await getInfoFromUrl(url)
-  return { width: dim.width, height: dim.height }
-}
-
-export default probeImageSize
