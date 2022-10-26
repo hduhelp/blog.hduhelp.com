876c9ae47afdc84c3cb5bfada0707e5e32e711f8
FDKevin <fdkevinzero@gmail.com>
2022-10-27 01:42:24 +0800

    updates

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,22 +1,18 @@
 import { Client } from '@notionhq/client'
 import { Env, getDatabase } from '../utils/notion'
-import { jsonResponse } from '../utils/response';
 
-export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
+export const onRequestGet: PagesFunction<Env> = async ({ request, waitUntil, env }) => {
   const url = new URL(request.url)
   let cache = await caches.open("custom:cache")
-  const cached = await cache.match(url.pathname)
-  if (cached) {
-    return cached
-  }
+  let response = await cache.match(url.pathname)
+  if (!response) {
+    const notion = new Client({ auth: env.NOTION_KEY })
+    const posts = await getDatabase(env.NOTION_DATABASE_ID, notion)
 
-  const notion = new Client({ auth: env.NOTION_KEY })
-  const posts = await getDatabase(env.NOTION_DATABASE_ID, notion)
-  const response = jsonResponse(posts, {
-    headers: {
-      "Cache-Control": "public, max-age=300, s-maxage=300",
-    }
-  })
-  await cache.put(url.pathname, response.clone())
+    response = new Response(JSON.stringify(posts), {
+      headers: { "Content-Type": "application/json", 'Cache-Control': 's-maxage=10' }
+    })
+    waitUntil(cache.put(url.pathname, response.clone()))
+  }
   return response
 };
